409778abbce412c4714d038689b84e25b2e71e2c
kdjdj106 <kdjdj106@naver.com>
2023-06-03 14:24:01 +0900

    GUEST -> USER

--- src/main/java/com/example/oauth2jwt/global/oauth2/OAuthAttributes.java
+++ src/main/java/com/example/oauth2jwt/global/oauth2/OAuthAttributes.java
@@ -82,7 +82,7 @@ public class OAuthAttributes {
                 .email(UUID.randomUUID() + "@socialUser.com")
                 .nickname(oauth2UserInfo.getNickname())
                 .imageUrl(oauth2UserInfo.getImageUrl())
-                .role(Role.GUEST)
+                .role(Role.USER)
                 .build();
     }
 }
